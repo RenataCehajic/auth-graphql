df2779ab6cc12d81ca193600c66db0acf0aa40a1
RenataCehajic <renata.cehajic@gmail.com>
2021-09-20 10:58:57 +0200

    Add Login and AuthForm component

--- client/components/App.js
+++ client/components/App.js
@@ -2,8 +2,12 @@ import React from "react";
 import Header from "./Header";
 
 const App = (props) => {
-  <Header />;
-  return <div>{props.children}</div>;
+  return (
+    <div className="container">
+      <Header />
+      {props.children}
+    </div>
+  );
 };
 
 export default App;
