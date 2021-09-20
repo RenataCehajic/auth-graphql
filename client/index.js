08c14d9dbef954ffce2269adc14622cfb3bd455b
RenataCehajic <renata.cehajic@gmail.com>
2021-09-20 10:24:00 +0200

    Login Form Design

--- client/index.js
+++ client/index.js
@@ -5,6 +5,7 @@ import { ApolloProvider } from "react-apollo";
 import { Router, hashHistory, Route, IndexRoute } from "react-router";
 
 import App from "./componnets/App";
+import LoginForm from "./components/LoginForm";
 
 const networkInterface = createNetworkInterface({
   uri: "/graphql",
@@ -22,7 +23,9 @@ const Root = () => {
   return;
   <ApolloProvider client={client}>
     <Router history={hashHistory}></Router>
-    <Route pathe="/" component={App}></Route>
+    <Route path="/" component={App}>
+      <Route path="login" component={LoginForm} />
+    </Route>
   </ApolloProvider>;
 };
 
